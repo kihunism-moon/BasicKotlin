9d4ec1eccb0f98afd5486460cc85222e743d7969
kihunism-moon <ymun893@gmail.com>
2021-05-24 17:26:44 +0900

    basis start001

--- app/src/main/java/com/example/basickotlin/01_basic/001_basis.kt
+++
@@ -1,5 +0,0 @@
-package com.example.basickotlin.`01_basic`
-
-fun main(args: Array<String>) {
-    println("basis")
-}
---
+++ app/src/main/java/com/example/basickotlin/basis001/001_basis.kt
@@ -0,0 +1,5 @@
+package com.example.basickotlin.basis001
+
+fun main(args: Array<String>) {
+    println("패키지 이름 시작은 영/소")
+}
